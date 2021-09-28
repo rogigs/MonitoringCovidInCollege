58bdafd16dbe2dd0e2aabeedca963cfc7e850320
Igor Gomes <igorgomes.13ig@gmail.com>
2021-09-28 19:07:27 -0300

    feat: add rule to Checkbox and button disabled

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -8,13 +8,18 @@ import CheckboxMUI from "../../components/Checkbox";
 import * as S from "./styles";
 
 const Login = () => {
-  const { handleSubmit, control } = useForm({
+  const { handleSubmit, control, watch } = useForm({
     mode: "onSubmit",
     defaultValues: {
       user: "",
       password: "",
+      keepMeConnected: false,
     },
   });
+
+  const { user, password } = watch();
+
+  const isDisabled = user === "" || password === "";
 
   const onSubmit = (data) => console.log(data);
 
@@ -36,16 +41,26 @@ const Login = () => {
             render={({ field }) => <TextFieldMUI label="Senha" {...field} />}
           />
         </S.WrapperField>
-        <CheckboxMUI label="Manter-me conectado" />
+        <Controller
+          name="keepMeConnected"
+          control={control}
+          render={({ field }) => (
+            <CheckboxMUI label="Manter-me conectado" field={field} {...field} />
+          )}
+        />
+
         <S.WrapperButton>
-          <ButtonMUI type="submit" name="Entrar" />
+          <ButtonMUI type="submit" disabled={isDisabled}>
+            Entrar
+          </ButtonMUI>
           <ButtonMUI
-            text
             className="centralize"
-            name="Cadastrar-se"
             size="small"
             disableElevation
-          />
+            variant="text"
+          >
+            Cadastrar-se
+          </ButtonMUI>
         </S.WrapperButton>
       </form>
     </S.Box>

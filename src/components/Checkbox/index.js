42e54ceb2fd20b17e32bf4eeb30407928f4e179e
Igor Gomes <igorgomes.13ig@gmail.com>
2021-09-28 19:06:58 -0300

    feat: add proporties to Checkbox

--- src/components/Checkbox/index.js
+++ src/components/Checkbox/index.js
@@ -3,12 +3,20 @@ import { Checkbox, FormControlLabel } from "@material-ui/core";
 import PropTypes from "prop-types";
 import React from "react";
 
-const CheckboxMUI = ({ label }, props) => (
-  <FormControlLabel control={<Checkbox />} label={label} {...props} />
+const CheckboxMUI = ({ label, onChange, value, props }) => (
+  <FormControlLabel
+    control={<Checkbox />}
+    onChange={(e) => onChange(e.target.checked)}
+    checked={value}
+    label={label}
+    {...props}
+  />
 );
 
 CheckboxMUI.propTypes = {
   label: PropTypes.string.isRequired,
+  onChange: PropTypes.func,
+  value: PropTypes.bool,
   props: PropTypes.shape(PropTypes.object),
 };
 

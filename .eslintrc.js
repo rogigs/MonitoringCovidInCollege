ce3fe05f2c417089468c29e8126c2f210d87a1c1
Igor Gomes <igorgomes.13ig@gmail.com>
2021-11-04 18:56:06 -0300

    feat: add yup in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
     "react/jsx-props-no-spreading": "off",
     "react/require-default-props": "off",
-    " react/forbid-prop-types": "off",
+    "react/forbid-prop-types": "off",
+    "import/prefer-default-export": "off",
   },
 };

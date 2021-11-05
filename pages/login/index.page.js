c9e6768b7e3c58c1c47c1719869c27602050d1ba
Igor Gomes <igorgomes.13ig@gmail.com>
2021-11-04 22:44:50 -0300

    feat: add modal in page login

--- pages/login/index.page.js
+++ pages/login/index.page.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/router";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -8,8 +8,16 @@ import CheckboxMUI from "~/components/Checkbox";
 import { authLogin } from "~/services/backend";
 import * as S from "./styles";
 import validationSchema from "./utils/validationSchema";
+import DialogMUI from "~/components/Dialog";
 
 const Login = () => {
+  const [modal, setModal] = useState({
+    open: false,
+    title: "",
+    message: "",
+    buttonName: "",
+  });
+
   const {
     handleSubmit,
     control,
@@ -24,10 +32,31 @@ const Login = () => {
     resolver: yupResolver(validationSchema),
   });
 
-  const onSubmit = (data) => {};
+  const router = useRouter();
 
+  const handleCloseModal = () => setModal({ ...modal, open: false });
+
+  const onSubmit = (data) => {
+    try {
+      router.push("/adm/dashboard");
+    } catch (error) {
+      setModal({
+        open: true,
+        title: "Erro ao logar",
+        message: "Nao foi possivel fazer o login, por favor tente novamente.",
+        buttonName: "Tentar novamente",
+      });
+    }
+  };
   return (
     <S.Box>
+      <DialogMUI
+        open={modal?.open}
+        onClose={handleCloseModal}
+        buttonName={modal?.buttonName}
+        title={modal?.title}
+        children={modal?.message}
+      />
       <form className="container" onSubmit={handleSubmit(onSubmit)}>
         <h1 className="title">COVID Monitoring</h1>
         <S.WrapperField>

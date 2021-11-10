98bcdd6eac04c5c7e9bb1ba2713831a755ce1817
Igor Gomes <igorgomes.13ig@gmail.com>
2021-11-10 15:56:57 -0300

    fix: error in login case auth incorrects

---
+++ pages/_app.page.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import Cookies from "universal-cookie";
+
+// This default export is required in a new `pages/_app.js` file.
+// eslint-disable-next-line react/prop-types
+const App = ({ Component, pageProps }) => {
+  const isBrowser = typeof window !== "undefined";
+  const router = useRouter();
+  const cookies = new Cookies();
+
+  const user = cookies.get("token");
+
+  useEffect(() => {
+    if (!user) {
+      return router.push("/login");
+    }
+  }, [user]);
+
+  return isBrowser ? <Component {...pageProps} /> : null;
+};
+
+export default App;
